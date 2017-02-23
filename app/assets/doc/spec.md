a472c01eb0c3916b712f12c03c899e40e03ce90f
Mo Bitar <me@bitar.io>
2017-02-23 05:29:15 -0600

    dl links 0.2.5, extensions copy

--- app/assets/doc/spec.md
+++ app/assets/doc/spec.md
@@ -37,9 +37,9 @@ To get started, download a Standard Notes app for your platform.
 
 - [Web](https://app.standardnotes.org)
 - [iOS](https://itunes.apple.com/us/app/id1191215138?mt=8)
-- [Mac](https://github.com/standardnotes/desktop/releases/download/v0.2.4/standard-notes-0.2.4-mac.zip)
-- [Windows](https://github.com/standardnotes/desktop/releases/download/v0.2.4/standard-notes-Setup-0.2.4.exe)
-- [Linux](https://github.com/standardnotes/desktop/releases/download/v0.2.4/standard-notes-0.2.4-x86_64.AppImage)
+- [Mac](https://github.com/standardnotes/desktop/releases/download/v0.2.5/standard-notes-0.2.5-mac.zip)
+- [Windows](https://github.com/standardnotes/desktop/releases/download/v0.2.5/standard-notes-Setup-0.2.5.exe)
+- [Linux](https://github.com/standardnotes/desktop/releases/download/v0.2.5/standard-notes-0.2.5-x86_64.AppImage)
 - Android ([In development](https://github.com/standardnotes/android))
 
 Then, you'll choose a Standard File server to host your data:
--- app/assets/javascripts/controllers/landing.js
+++ app/assets/javascripts/controllers/landing.js
@@ -2,7 +2,7 @@ class LandingCtrl {
   constructor($rootScope, $scope, $timeout) {
 
     $scope.displayData = {};
-    let currentDesktopVersion = "0.2.4";
+    let currentDesktopVersion = "0.2.5";
 
     document.onkeydown = function(e) {
       $timeout(function(){
@@ -43,7 +43,7 @@ class LandingCtrl {
       {
         name: "Mac",
         icon: "ico-apple.png",
-        link: "https://github.com/standardnotes/desktop/releases/download/v0.2.4/standard-notes-0.2.4-mac.zip",
+        link: "https://github.com/standardnotes/desktop/releases/download/v0.2.5/standard-notes-0.2.5-mac.zip",
         screenshots: [
           "https://s3.amazonaws.com/standard-notes/screenshots/mac/jpg/1.jpg",
           "https://s3.amazonaws.com/standard-notes/screenshots/mac/jpg/2.jpg",
@@ -55,7 +55,7 @@ class LandingCtrl {
       {
         name: "Windows",
         icon: "ico-windows.png",
-        link: "https://github.com/standardnotes/desktop/releases/download/v0.2.4/standard-notes-Setup-0.2.4.exe",
+        link: "https://github.com/standardnotes/desktop/releases/download/v0.2.5/standard-notes-Setup-0.2.5.exe",
         screenshots: [
           "https://s3.amazonaws.com/standard-notes/screenshots/desktop/jpg/1.jpg",
           "https://s3.amazonaws.com/standard-notes/screenshots/desktop/jpg/2.jpg",
@@ -67,7 +67,7 @@ class LandingCtrl {
       {
         name: "Linux",
         icon: "ico-linux.png",
-        link: "https://github.com/standardnotes/desktop/releases/download/v0.2.4/standard-notes-0.2.4-x86_64.AppImage",
+        link: "https://github.com/standardnotes/desktop/releases/download/v0.2.5/standard-notes-0.2.5-x86_64.AppImage",
         screenshots: [
           "https://s3.amazonaws.com/standard-notes/screenshots/desktop/jpg/1.jpg",
           "https://s3.amazonaws.com/standard-notes/screenshots/desktop/jpg/2.jpg",
